c369059560a2af564657595d0af45f89d1253a26
Maxim Lyasnikov <vfrc29@gmail.com>
2020-07-01 20:59:59 +0300

    add keypad decoder

--- Src/main.c
+++ Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "keypad.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
---
+++ lib/keypad/keypad.h
@@ -0,0 +1,46 @@
+/**
+ * @file
+ * #brief Module to read matrix 4x3 telephone keyboard
+ */
+
+#ifndef __keypad_H
+#define __keypad_H
+
+#include <stdint.h>
+
+// R1 - *   *   *   * 1
+// R2 - *   *   *   * 2
+// R3 - *   *   *   * 4
+// R4 - *   +   *   * 8
+//      |   |   |   |
+//     C1  C2  C3  C4
+//      1   2   4   8
+
+// R1 R2 R3 R4 C1 C2 C3 C4
+
+static const char scanCodes[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'},
+};
+
+typedef enum
+{
+    KEYPAD_READ_VARIANT_COLUMN,
+    KEYPAD_READ_VARIANT_ROWS,
+} KeypadReadVariantEnum;
+
+typedef uint8_t (*KeypadReadPortCb)(KeypadReadVariantEnum);
+
+typedef struct {
+    KeypadReadPortCb readPortCb;
+} KeypadType;
+
+int keypad_init(KeypadType *keypadDef);
+
+char keypad_getkey(KeypadType *keypadDef);
+int keypad_getkeys(KeypadType *keypadDef, char *outBuffer, int count);
+
+
+#endif // __keypad_H
